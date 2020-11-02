d32a828d7b053c4b4a169afbea231af0e9e4d6a4
namrataM <911npm@gmail.com>
2020-11-02 17:54:33 +0530

    :art: Refactored code

--- src/main/java/com/tdd/kata/StringCalculator.java
+++ src/main/java/com/tdd/kata/StringCalculator.java
@@ -27,8 +27,17 @@ public class StringCalculator {
             return Integer.parseInt(split[0]);
         }
 
-        List<Integer> negatives = Arrays.stream(split)
-                    .mapToInt(Integer::parseInt)
+        return calculateSum(split);
+    }
+
+    /**
+     * Method to return sum or throw exception for negative numbers in input
+     * @throws NegativeInputException for negative numbers in input
+     * @return sum
+     */
+    private int calculateSum(String[] numbers){
+        List<Integer> negatives = Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
                 .filter(value -> value < 0)
                 .boxed()
                 .collect(Collectors.toList());
@@ -41,7 +50,7 @@ public class StringCalculator {
             throw new NegativeInputException("Negative numbers in input: "+ sb.toString());
         }
 
-        int sum = Arrays.stream(split).mapToInt(Integer::parseInt).sum();
+        int sum = Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
 
         return sum;
     }
